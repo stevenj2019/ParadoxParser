317f5669cc2341c56b979271fb884dadb66418de
stevenj2019 <steven.j2019@pm.me>
2026-02-27 18:32:06 +0000

    removed setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-#apparnetly depcrecated.
-# from setuptools import setup, find_packages
-
-# setup(
-#     name = "ParadoxParser",
-#     version=1.0,
-#     python_requires=">=3.6",#idk what this value should be, i dont think itll actually matter?
-#     packages=find_packages()
-# )
