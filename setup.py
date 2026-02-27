c4707e0ef135afe0feea15ec2d4741caf74bdbaf
stevenj2019 <steven.j2019@pm.me>
2026-02-27 17:55:27 +0000

    attempt the third

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup, find_packages
+#apparnetly depcrecated.
+# from setuptools import setup, find_packages
 
-setup(
-    name = "ParadoxParser",
-    version=1.0,
-    python_requires=">=3.6",#idk what this value should be, i dont think itll actually matter?
-    packages=find_packages()
-)
+# setup(
+#     name = "ParadoxParser",
+#     version=1.0,
+#     python_requires=">=3.6",#idk what this value should be, i dont think itll actually matter?
+#     packages=find_packages()
+# )
