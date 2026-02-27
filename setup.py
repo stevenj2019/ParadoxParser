fd699972022da3f1e256d4c6ab99922fb599fa47
stevenj2019 <steven.j2019@pm.me>
2026-02-27 17:17:15 +0000

    added setup for pip (maybe?)

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "ParadoxParser",
+    version=1.0,
+    python_requires=">=3.6",#idk what this value should be, i dont think itll actually matter?
+    packages=find_packages()
+)
