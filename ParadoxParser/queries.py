96df197797fb51013c3325696428399ab8111695
stevenj2019 <steven.j2019@pm.me>
2026-08-04 22:54:15 +0100

    .key not .id (facepalm)

--- ParadoxParser/queries.py
+++ ParadoxParser/queries.py
@@ -8,24 +8,24 @@ class NodeContainer(Protocol):
     nodes: list
 
 def find_block(block:NodeContainer, match_id:str) -> GenericBlock|None:
-    return next((node for node in block.nodes if isinstance(node, GenericBlock) and node.id == match_id), None)
+    return next((node for node in block.nodes if isinstance(node, GenericBlock) and node.key == match_id), None)
 
 def find_blocks(block:NodeContainer, match_id:str) -> list[GenericBlock]:
-    return [node for node in block.nodes if isinstance(node, GenericBlock) and node.id == match_id]
+    return [node for node in block.nodes if isinstance(node, GenericBlock) and node.key == match_id]
 
 def find_keyvalue(block:NodeContainer, match_id:str) -> GenericBlock|None:
-    return next((node for node in block.nodes if isinstance(node, GenericKeyValue) and node.id == match_id), None)
+    return next((node for node in block.nodes if isinstance(node, GenericKeyValue) and node.key == match_id), None)
 
 def all_blocks(block:NodeContainer, match_id:str|None = None) -> Iterator[GenericBlock]:
     for node in block.nodes:
         if not isinstance(node, GenericBlock):
             continue
-        if match_id is None or node.id == match_id:
+        if match_id is None or node.key == match_id:
             yield node
 
 def all_keyvalues(block:NodeContainer, match_id:str|None = None) -> Iterator[GenericKeyValue]:
     for node in block.nodes:
         if not isinstance(node, GenericKeyValue):
             continue
-        if match_id is None or node.id == match_id:
+        if match_id is None or node.key == match_id:
             yield node
